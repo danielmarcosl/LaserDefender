391970d37c31343064cfe988d0342cadb3f99bb8
Daniel <danielmarcos.to@gmail.com>
2016-01-23 19:00:57 +0000

    Delete several lines of code of LevelManager of the previous project

--- Assets/Scrips/LevelManager.cs
+++ Assets/Scrips/LevelManager.cs
@@ -5,19 +5,11 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
-		Brick.breakableCount = 0;
 		SceneManager.LoadScene (name);
 	}
 
 	public void LoadNextLevel () {
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
-		Brick.breakableCount = 0;
-	}
-
-	public void BrickDestroyed () {
-		if (Brick.breakableCount <= 0) {
-			LoadNextLevel ();
-		}
 	}
 
 	public void QuitRequest () {
