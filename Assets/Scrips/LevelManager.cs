6b3c68f15e4613ee93deb404386b85f39da32465
Daniel <danielmarcos.to@gmail.com>
2016-01-23 14:02:06 +0000

    Added common assets from BlockBreaker

---
+++ Assets/Scrips/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Brick.breakableCount = 0;
+		SceneManager.LoadScene (name);
+	}
+
+	public void LoadNextLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		Brick.breakableCount = 0;
+	}
+
+	public void BrickDestroyed () {
+		if (Brick.breakableCount <= 0) {
+			LoadNextLevel ();
+		}
+	}
+
+	public void QuitRequest () {
+		Application.Quit ();
+	}
+}
