d700e1ffdec3ecd81e609c83183dfeb20d82719b
Daniel <danielmarcos.to@gmail.com>
2016-01-24 11:24:27 +0000

    Restrict the position of the player ship

--- Assets/Scrips/PlayerControler.cs
+++ Assets/Scrips/PlayerControler.cs
@@ -3,9 +3,27 @@ using System.Collections;
 
 public class PlayerControler : MonoBehaviour {
 
+	float xmin;
+	float xmax;
+	float ymin;
+	float ymax;
+
 	// Use this for initialization
 	void Start () {
-	
+		restrictPosition ();
+	}
+
+	void restrictPosition() {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+		Vector3 upmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distance));
+		Vector3 downmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+
+		xmin = leftmost.x + 0.6f;
+		xmax = rightmost.x - 0.6f;
+		ymin = downmost.y + 0.5f;
+		ymax = upmost.y - 0.5f;
 	}
 
 	// Update is called once per frame
@@ -27,5 +45,10 @@ public class PlayerControler : MonoBehaviour {
 		} else if (Input.GetKey (KeyCode.DownArrow)) { // Down movement
 			gameObject.transform.position += Vector3.down * speed * Time.deltaTime;
 		}
+
+		// Restrict the player to the gamespace
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		float newY = Mathf.Clamp (transform.position.y, ymin, ymax);
+		transform.position = new Vector3 (newX, newY, transform.position.z);
 	}
 }
