9d14086858bd2dd0e91ae168e2eb50b1d7522a33
Daniel <danielmarcos.to@gmail.com>
2016-01-25 13:43:19 +0000

    Order code

--- Assets/Entity/EnemyFormation/EnemySpawner.cs
+++ Assets/Entity/EnemyFormation/EnemySpawner.cs
@@ -14,35 +14,45 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
-		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceToCamera));
-		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceToCamera));
+		setBoundaries ();
+		instantiateEnemies ();
+	}
 
-		xmin = leftBoundary.x;
-		xmax = rightBoundary.x;
-
+	public void instantiateEnemies () {
 		foreach (Transform child in transform) {
 			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
 		}
 	}
 
-	public void OnDrawGizmos() {
+	public void setBoundaries () {
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceToCamera));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceToCamera));
+
+		xmin = leftBoundary.x;
+		xmax = rightBoundary.x;
+	}
+
+	public void OnDrawGizmos () {
 		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		enemyMovement ();
+		changeDirection ();
 	}
 
-	public void enemyMovement() {
+	public void enemyMovement () {
 		if (movingRight) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		} else {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
+	}
 
+	public void changeDirection() {
 		float rightEdgeOfFormation = transform.position.x + (0.5f * width);
 		float leftEdgeOfFormation = transform.position.x - (0.5f * width);
 
