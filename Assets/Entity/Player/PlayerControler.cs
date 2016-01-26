92d683d82718173bb94b4a18e26a716951cd4159
Daniel <danielmarcos.to@gmail.com>
2016-01-26 14:03:49 +0000

    The player now is not autodestroyed by his lasers

--- Assets/Entity/Player/PlayerControler.cs
+++ Assets/Entity/Player/PlayerControler.cs
@@ -69,7 +69,8 @@ public class PlayerControler : MonoBehaviour {
 	}
 
 	void Fire () {
-		GameObject beam = Instantiate (projectile, transform.position, Quaternion.identity) as GameObject;
+		Vector3 offset = new Vector3 (0, 1, 0);
+		GameObject beam = Instantiate (projectile, transform.position + offset, Quaternion.identity) as GameObject;
 		beam.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, projectileSpeed,0);
 	}
 
